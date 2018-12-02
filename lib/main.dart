e0048568ce5b2cff24bc7f3bd27b391e8a633e1b
Jason Franklin <jason.franklin@xing.com>
2018-12-02 09:52:32 +0100

    Initial project import

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  return runApp(new UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
